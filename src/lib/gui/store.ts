95ce71e9f4c37ecce9df865b8723ff04a770864b
onsclom <onsclom@onsclom.net>
2023-11-01 21:18:23 -0700

    Add types

--- src/lib/gui/store.ts
+++ src/lib/gui/store.ts
@@ -1,5 +1,8 @@
 import { writable } from 'svelte/store'
 
-export function guiControls(values: any) {
-	return writable(values)
+type Range = { value: number; min: number; max: number; step: number }
+type Controls = Record<string, number | string | boolean | Range>
+
+export function guiControls<T extends Controls>(values: T) {
+  return writable(values)
 }
